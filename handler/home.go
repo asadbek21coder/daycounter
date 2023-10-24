e27c55d90ec0578c7c97d76ee6abbf24e843c395
asadbek21coder <asadbek21coder@gmail.com>
2023-10-24 13:57:59 +0500

    rm index.html

--- handler/home.go
+++ handler/home.go
@@ -1,32 +1,45 @@
 package handler
 
 import (
-	"html/template"
+	"fmt"
 	"net/http"
 	"time"
 )
-
-type TemplateData struct {
-	DaysRemaining int
-}
 
 func GetHomeHandler(w http.ResponseWriter, r *http.Request) {
 	target := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
 	remain := int(target.Sub(time.Now()).Hours() / 24)
 
-	data := TemplateData{DaysRemaining: remain}
-
-	tmpl, err := template.ParseFiles("templates/index.html")
-
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	temp := `<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Days Remaining</title>
+	</head>
+	<style>
+	html, body, h1, h2 {
+	  margin: 0;
+	  padding: 0;
 	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	
+	.header {
+	  background-color: #333;
+	  color: #fff;
+	  padding: 10px;
+	  text-align: center;
 	}
+	.container {
+	  max-width: 300px;
+	  margin: 0 auto;
+	  padding: 100px;
+	}
+	
+	</style>
+	<body>
+			<h1 class="header">Days Remaining Until January 1, 2025</h1>
+			<h2 class="container">There are <b><i style="font-size: xx-large;">` + fmt.Sprint(remain) + `</i></b> days left.</h2>
+	</body>
+	</html>`
 
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, temp)
 }
